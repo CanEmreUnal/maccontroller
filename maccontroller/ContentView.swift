846e1aa90709618c8561ab16323fb4e75b6e45f9
canemreunal <canemreunal@hotmail.com>
2024-06-08 18:28:43 +0300

    Initial Commit

---
+++ maccontroller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  maccontroller
+//
+//  Created by CAN EMRE ÜNAL on 8.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ maccontroller/maccontrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  maccontrollerApp.swift
+//  maccontroller
+//
+//  Created by CAN EMRE ÜNAL on 8.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct maccontrollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
